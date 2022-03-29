ee2a09186df2166ebd7b4bc212556aeeadc1a5f2
Dilara Bülbül <97522259+DBulbul@users.noreply.github.com>
2022-03-29 11:57:57 +0300

    Update TaksimetreProgrami.java

--- Java101/src/com/company/TaksimetreProgrami.java
+++ Java101/src/com/company/TaksimetreProgrami.java
@@ -1,2 +1,12 @@
 package com.company;public class TaksimetreProgrami {
+  public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Mesafeyi KM cinsinden giriniz:");
+        int KM=input.nextInt();
+        double odemeTaksi=(10+2.2*KM);
+        double taksimetre=(odemeTaksi<20)?20:odemeTaksi;
+        System.out.println("Ödenecek tutar"+taksimetre);
+    }
 }
+
+
